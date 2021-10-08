247277f44392c49ec590976659327176fb2c5f6b
marcosarango <sar21005@byui.edu>
2021-10-07 21:34:18 -0400

    h_l funtion

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -1,6 +1,25 @@
+import random as rd
 class Player:
     def __init__(self):
-        self.guesses = 0
+        self.guesses = rd.randint(1, 13)
+        self.card_of_player = 0
+        self.score = 0
+    
+    def choosing_h_l(self, h_l):
+        if self.guesses < self.card_of_player and h_l == "l":
+            self.score += 100
+        elif self.guesses > self.card_of_player and h_l == "h":
+            self.score += 100
+        elif self.guesses > self.card_of_player and h_l == "l":
+            self.score-= 75
+        elif self.guesses < self.card_of_player and h_l == "h":
+            self.score-= 75
+        else:
+            self.score = 0            
+        return self.score    
+
+
+
 
 
 Player()
